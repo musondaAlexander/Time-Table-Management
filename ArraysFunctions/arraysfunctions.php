42821a1ea67a9a4320e252e583474cac788f6775
musondaalexander <musondaalexander97@gmail.com>
2022-10-06 01:44:47 +0200

    modifield all the for files the connection to the remote db

--- ArraysFunctions/arraysfunctions.php
+++ ArraysFunctions/arraysfunctions.php
@@ -214,26 +214,21 @@
 	var arraysubject =
 	"<?php	
 
-      /* From heroku to the server  */
-	  //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-//$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);v
+      /* From heroku to the server
+	    $servername = "localhost";
+		$username = "alex";
+		$password = "alEx@2022@zuct";
+		$dbname = "timetabledata";
+	 
+	 
+	  */
 
-
-
-/** for local development */
-		$servername = $cleardb_server;
-		$username = $cleardb_username;
-		$password = $cleardb_password;
-		$dbname = $cleardb_db;
-		$conn = new mysqli($GLOBALS['servername'],$GLOBALS['username'],$GLOBALS['password'],$dbname);
+	
+	  $servername = "us-cdbr-east-06.cleardb.net";
+		$username = "beb5e1d2cd0e49";
+		$password = "a60fad52";
+		$dbname = "heroku_3ca43a23c94a4d6";
+		$conn = new mysqli($GLOBALS['servername'],$GLOBALS['username'],$GLOBALS['password'],"heroku_3ca43a23c94a4d6");
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
 		}
--- Department/Department.php
+++ Department/Department.php
@@ -4,26 +4,20 @@
 	// $username = "alex";
 	// $password = "alEx@2022@zuct";
 	// $dbname = "timetabledata";
-	
+
          /* From heroku to the server  */
 	  //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
+
 // Connect to DB
 //$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);v
 
 
 
 /** for local development */
-		$servername = $cleardb_server;
-		$username = $cleardb_username;
-		$password = $cleardb_password;
-		$dbname = $cleardb_db;
+        $servername = "us-cdbr-east-06.cleardb.net";
+		$username = "beb5e1d2cd0e49";
+		$password = "a60fad52";
+		$dbname = "heroku_3ca43a23c94a4d6";
 
 	$conn = new mysqli($servername,$username,$password,$dbname);
 	if ($conn->connect_error) {
